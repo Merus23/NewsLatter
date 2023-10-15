f7dd51b20b1208f1c1d9c3a123ced6ca63842c25
Merus23 <mateus.satira@gmail.com>
2023-10-15 18:59:17 -0300

    feat: create the NewsLatter structure

--- src/components/NewsLatter/NewsLatter.jsx
+++ src/components/NewsLatter/NewsLatter.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
+import { useState } from 'react'
 
 const NewsLatter = () => {
-  return (
-    <div>
-      <h1>NewsLatter</h1>
-    </div>
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+
+  
+    return (
+    <>
+        <form action="">
+
+            <h1>NewsLatter</h1>
+            <p>Explore nossa interface de newsletter intuitiva, perfeita para receber as últimas notícias diretamente em sua caixa de entrada! Desfrute da integração fácil em seus aplicativos, personalização de design e gerenciamento eficiente de assinaturas. Mantenha-se atualizado de forma simples e conveniente!"</p>
+           <div>
+                
+                <label>
+                    <span>Nome:</span>
+                    <input type="text" name="name" required placeholder='Seu nome' value={email} onChange={(e) => setEmail(e.target.value)} />
+                </label>
+                
+                <label>
+                    <span>E-mail:</span>
+                    <input type="email" name="email" required placeholder='Seu e-mail' value={email} onChange={(e) => setEmail(e.target.value)} />
+                </label>
+                
+                <button type="submit">Participar</button>
+           </div> 
+        </form>
+
+    </>
+    
   )
 }
 
